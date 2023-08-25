4bcd4cb3cee08c8e1fe1798a405255fe0a77fa11
Lucas Lean <lucaslean@outlook.com.br>
2023-08-25 16:32:30 -0300

    (o゜▽゜)o☆ Altera descrições de todas pericias, vantagens e desvantagens.

--- src/data/pericias.js
+++ src/data/pericias.js
@@ -1,76 +1,50 @@
 export const Pericias = [
     {
         Nome: "Animais",
-        Descricao: `Você sabe cuidar, adestrar, cavalgar e lidar com animais e outras criaturas
-        irracionais. Pode substituir Medicina (apenas para animais). Com bons resultados nos testes,
-        você consegue até se comunicar com animais.`
+        Descricao: "Você possui habilidades em cuidar, adestrar, cavalgar e lidar com animais e outras criaturas irracionais. Pode substituir Medicina (apenas para animais). Com sucesso em testes, pode até se comunicar com animais."
     },
     {
         Nome: "Arte",
-        Descricao: `Você sabe fazer performances artísticas como cantar, dançar, tocar música, cozinhar,
-        fazer cosplay (e se disfarçar), desenhar, avaliar objetos de arte e outras.`
+        Descricao: "Você é hábil em performances artísticas como cantar, dançar, tocar música, cozinhar, fazer cosplay (e se disfarçar), desenhar e avaliar objetos de arte, entre outras atividades."
     },
     {
         Nome: "Esporte",
-        Descricao: `Você conhece os muitos tipos de esportes e suas regras, além de ser capacitado
-        em atividades físicas como correr, escalar, nadar, fazer acrobacias, equilibrar-se, saltar e
-        outras.`
+        Descricao: "Você possui amplo conhecimento sobre vários esportes e suas regras, além de ser habilidoso em atividades físicas como correr, escalar, nadar, realizar acrobacias, equilibrar-se, saltar e outras ações similares."
     },
     {
         Nome: "Influência",
-        Descricao: `Você sabe convencer outros a acreditar em algo ou fazer o que você quer.
-        Envolve coisas como diplomacia, liderança, intimidação, sedução, blefe, hipnose, lábia,
-        barganha, obter informações e outros.`
+        Descricao: "Você é capaz de persuadir outros a acreditar ou fazer o que você deseja. Isso inclui diplomacia, liderança, intimidação, sedução, blefe, hipnose, lábia, barganha, obtenção de informações e outras técnicas de influência."
     },
     {
         Nome: "Luta",
-        Descricao: `Você sabe atacar e se defender em combate, seja corpo a corpo ou à distância ou...
-        Ah, você já anotou na ficha. Deixa pra lá.`
+        Descricao: "Você é habilidoso em combate, seja corpo a corpo, à distância ou... bem, você já anotou isso na ficha, não é? Vamos deixar assim."
     },
     {
         Nome: "Manha",
-        Descricao: `Você sabe fazer coisas malandras ou ilegais, como construir (e sabotar)
-        armadilhas, arrombar portas e fechaduras, bater carteiras, criar (e decifrar) mensagens
-        criptografadas, se disfarçar, falsificar objetos, ser furtivo, intimidar, rastrear pistas e
-        pegadas…`
+        Descricao: "Você é talentoso em realizar ações maliciosas ou ilegais, como construir (e sabotar) armadilhas, arrombar portas e fechaduras, praticar carteirismo, criar (e decifrar) mensagens criptografadas, se disfarçar, falsificar objetos, agir furtivamente, intimidar, rastrear pistas e pegadas, entre outras habilidades similares."
     },
     {
         Nome: "Máquinas",
-        Descricao: `Você sabe operar, construir e consertar máquinas, veículos e aparelhos de
-        todo tipo. Também sabe lidar com computadores, hackear sistemas, vasculhar a internet,
-        jogar games ou agir em simulações e realidades virtuais. Pode substituir Medicina (apenas
-        para construtos).`
+        Descricao: "Você possui habilidades em operar, construir e consertar máquinas, veículos e aparelhos de diversos tipos. Também é experiente em lidar com computadores, hackear sistemas, explorar a internet, jogar games e atuar em simulações e realidades virtuais. Pode substituir Medicina (apenas para construtos)."
     },
     {
         Nome: "Medicina",
-        Descricao: `Você sabe realizar primeiros socorros, diagnósticos, tratar doenças e venenos,
-        realizar cirurgias e todo tipo de conhecimento de saúde. Pode despertar um personagem
-        inconsciente ou estabilizar um personagem quase morto.`
+        Descricao: "Você possui conhecimento em primeiros socorros, diagnósticos, tratamento de doenças e venenos, e é capaz de realizar cirurgias e outras técnicas de cuidados de saúde. Pode reanimar um personagem inconsciente ou estabilizar um personagem quase morto."
     },
     {
         Nome: "Mística",
-        Descricao: `Você sabe sobre forças sobrenaturais e artes místicas. Quando atacar ou se
-        defender com poderes mágicos ou sobrenaturais, use esta perícia. Também é usada para
-        reconhecer, contra-atacar e teorizar sobre conhecimentos ocultos, magia e criaturas mágicas.`
+        Descricao: "Você possui conhecimento sobre forças sobrenaturais e artes místicas. Quando ataca ou se defende com poderes mágicos ou sobrenaturais, utiliza essa perícia. Também é usada para identificar, contrapor e teorizar sobre conhecimentos ocultos, magia e criaturas mágicas."
     },
     {
         Nome: "Percepção",
-        Descricao: `Você sabe usar seus sentidos para perceber melhor o mundo ao redor.
-        Usada para ouvir ruídos muito baixos, notar coisas e personagens distantes, escondidos ou
-        sob disfarce, ler lábios, rastrear pistas, evitar ser surpreendido e até mesmo como um “sexto
-        sentido” para tentar notar se alguém está mentindo ou agindo de forma suspeita.`
+        Descricao: "Você é habilidoso em utilizar seus sentidos para perceber o mundo ao seu redor. Pode ouvir ruídos imperceptíveis, detectar coisas e personagens distantes, ocultos ou disfarçados, fazer leitura labial, rastrear pistas, evitar ser surpreendido e até mesmo utilizar um 'sexto sentido' para identificar mentiras ou comportamentos suspeitos."
     },
     {
         Nome: "Saber",
-        Descricao: `Você sabe tudo sobre tudo, qualquer conhecimento teórico em ciências, idiomas e
-        até assuntos sobrenaturais, ou como e onde pesquisá-los. Note que é uma perícia ampla,
-        engloba todos os campos, própria para aqueles cientistas exagerados ou super nerds que
-        sabem de tudo!`
+        Descricao: "Você possui um vasto conhecimento teórico em ciências, idiomas e até mesmo assuntos sobrenaturais, ou sabe onde pesquisar tais informações. Esta é uma perícia ampla, abrangendo diversos campos de conhecimento, sendo adequada para cientistas ávidos ou fanáticos por conhecimento que sabem um pouco sobre tudo!"
     },
     {
         Nome: "Sustento",
-        Descricao: `Você sabe subsistir e se orientar em condições adversas. Pode encontrar e
-        produzir alimento, construir abrigos, rastrear pistas, reconhecer criaturas selvagens,
-        construir e sabotar armadilhas, ser furtivo, nadar, prever o clima, entre outros.`
+        Descricao: "Você é capaz de sobreviver e se orientar em situações adversas. Pode encontrar e produzir alimentos, construir abrigos, rastrear pistas, identificar criaturas selvagens, criar e desarmar armadilhas, ser furtivo, nadar, prever condições climáticas e muito mais."
     }
 ]
