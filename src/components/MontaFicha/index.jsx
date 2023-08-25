528e2a61a5ad97f19bc9757898eebd9eabc86f5b
Mauricio Soares <kratoscheky@gmail.com>
2023-08-25 16:33:10 -0300

    Removida infos temporariamente

--- src/components/MontaFicha/index.jsx
+++ src/components/MontaFicha/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useFicha } from "../../context/ficha.context";
 import { ContainerMontaFicha, TextArea } from "./styles";
-import FichaInput from '../FichaInput'
-import dadinho from '../../images/dadinhos.png'
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import FichaInput from "../FichaInput";
+import dadinho from "../../images/dadinhos.png";
+import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
 import ModalPericias from "../ModalPericias";
 import ModalVantagens from "../ModalVantagens";
 import ModalDesvantagens from "../ModalDesvantagens";
@@ -21,44 +21,86 @@ export const MontaFicha = () => {
     setDesvantagens,
     periciais,
     setPericias,
-    HandleAtributos
+    HandleAtributos,
   } = useFicha();
 
   const [addPericiasOpen, setAddPericiasOpen] = useState(false);
   const [addVantagensOpen, setAddVantagensOpen] = useState(false);
   const [addDesvantagensOpen, setAddDesvantagensOpen] = useState(false);
 
-  const SeparadorDadinho = () => <img src={dadinho} alt={'Dados'} />;
+  const SeparadorDadinho = () => <img src={dadinho} alt={"Dados"} />;
 
   return (
     <>
       <ContainerMontaFicha>
         <h1>Dados Basicos</h1>
-        <FichaInput label={'Nome'} valor={nome} onEdit={(e) => setNome(e.target.value)} />
-        <FichaInput label={'Descrição'} valor={detalhes} onEdit={(e) => setDetalhes(e.target.value)} maxLength={250} />
+        <FichaInput
+          label={"Nome"}
+          valor={nome}
+          onEdit={(e) => setNome(e.target.value)}
+        />
+        <FichaInput
+          label={"Descrição"}
+          valor={detalhes}
+          onEdit={(e) => setDetalhes(e.target.value)}
+          maxLength={250}
+        />
         <SeparadorDadinho />
         <h1>Atributos</h1>
-        <FichaInput label={'Poder'} valor={atributos.poder} onEdit={(v) => HandleAtributos(v.target.value, "poder")} />
-        <FichaInput label={'Habilidade'} valor={atributos.habilidade} onEdit={(v) => HandleAtributos(v.target.value, "habilidade")} />
-        <FichaInput label={'Resistência'} valor={atributos.resistencia} onEdit={(v) => HandleAtributos(v.target.value, "resistencia")} />
+        <FichaInput
+          label={"Poder"}
+          valor={atributos.poder}
+          onEdit={(v) => HandleAtributos(v.target.value, "poder")}
+        />
+        <FichaInput
+          label={"Habilidade"}
+          valor={atributos.habilidade}
+          onEdit={(v) => HandleAtributos(v.target.value, "habilidade")}
+        />
+        <FichaInput
+          label={"Resistência"}
+          valor={atributos.resistencia}
+          onEdit={(v) => HandleAtributos(v.target.value, "resistencia")}
+        />
         <SeparadorDadinho />
         <h1>Personalidade</h1>
         <div>
-          <label style={{display: 'flex', alignItems: 'center', gap: '4px'}}>Perícias <InfoRoundedIcon style={{width: '15px'}} onClick={() => setAddPericiasOpen(true)} />:</label>
+          <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+            Perícias{" "}
+            {/* <InfoRoundedIcon
+              style={{ width: "15px" }}
+              onClick={() => setAddPericiasOpen(true)}
+            /> */}
+            :
+          </label>
           <TextArea
             value={periciais}
             onChange={(v) => setPericias(v.target.value)}
           ></TextArea>
         </div>
         <div>
-          <label style={{display: 'flex', alignItems: 'center', gap: '4px'}}>Vantagens <InfoRoundedIcon style={{width: '15px'}} onClick={() => setAddVantagensOpen(true)} />:</label>
+          <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+            Vantagens
+            {/* <InfoRoundedIcon
+              style={{ width: "15px" }}
+              onClick={() => setAddVantagensOpen(true)}
+            /> */}
+            :
+          </label>
           <TextArea
             value={vantagens}
             onChange={(v) => setVantagens(v.target.value)}
           ></TextArea>
         </div>
         <div>
-          <label style={{display: 'flex', alignItems: 'center', gap: '4px'}}>Desvantagens <InfoRoundedIcon style={{width: '15px'}} onClick={() => setAddDesvantagensOpen(true)} />:</label>
+          <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+            Desvantagens{" "}
+            {/* <InfoRoundedIcon
+              style={{ width: "15px" }}
+              onClick={() => setAddDesvantagensOpen(true)}
+            /> */}
+            :
+          </label>
           <TextArea
             value={desvantagens}
             onChange={(v) => setDesvantagens(v.target.value)}
@@ -66,9 +108,18 @@ export const MontaFicha = () => {
         </div>
       </ContainerMontaFicha>
 
-      <ModalPericias open={addPericiasOpen} handleClose={() => setAddPericiasOpen(false)} />
-      <ModalVantagens open={addVantagensOpen} handleClose={() => setAddVantagensOpen(false)} />
-      <ModalDesvantagens open={addDesvantagensOpen} handleClose={() => setAddDesvantagensOpen(false)} />
+      <ModalPericias
+        open={addPericiasOpen}
+        handleClose={() => setAddPericiasOpen(false)}
+      />
+      <ModalVantagens
+        open={addVantagensOpen}
+        handleClose={() => setAddVantagensOpen(false)}
+      />
+      <ModalDesvantagens
+        open={addDesvantagensOpen}
+        handleClose={() => setAddDesvantagensOpen(false)}
+      />
     </>
   );
 };
