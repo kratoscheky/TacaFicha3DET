317b8181cbb235e25ebb4182c410c0f830f99e71
Mauricio Soares <kratoscheky@gmail.com>
2023-08-25 13:11:52 -0300

    Creditos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
-import { Topo } from './components/Topo';
-import { Ficha } from './pages/Ficha';
-import { ContainerPage } from './styles';
+import { display } from "@mui/system";
+import "./App.css";
+import { Topo } from "./components/Topo";
+import { Ficha } from "./pages/Ficha";
+import { ContainerPage } from "./styles";
 
 function App() {
   return (
-    <ContainerPage>
-      <Topo />
-      <Ficha />
-    </ContainerPage>
+    <>
+      <ContainerPage>
+        <Topo />
+        <Ficha />
+      </ContainerPage>
+      <div
+        style={{
+          background: "#000",
+          width: "100%",
+          height: "75px",
+        }}
+      >
+        <p style={{
+          color: '#FFF',
+          display: 'flex',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '4px',
+        }}>Este site foi criado por <a style={{color: '#D11CE0'}} href='https://twitter.com/Kratoscheky'>@kratoscheky</a> com colaboração de <a style={{color: '#D11CE0'}} href='https://twitter.com/OvelhaDev'>@OvelhaDev</a></p>
+      </div>
+    </>
   );
 }
 
