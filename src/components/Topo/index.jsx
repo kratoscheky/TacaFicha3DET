2a0446efb28941ed58ac5d327f5c4f164c360192
Mauricio Soares <kratoscheky@gmail.com>
2023-08-27 17:31:01 -0300

    Alinhamento do topo

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -7,12 +7,18 @@ export const Topo = () => {
             <div style={{width: '100%', backgroundColor: '#000'}}>
             <img style={{height: '50px', paddingLeft: '8px', paddingTop: '8px'}} src={logo} />
             </div>
-            <div style={{padding: '8px 8px', display: 'flex', flexDirection: 'column', maxWidth: '500px'}}>
-                <h1 style={{textTransform: 'uppercase'}}>3DeT tacaficha</h1>
-                <br />
-                <p style={{color: '#7A7A7A'}}>Este é um site não oficial para auxiliar na criação de personagens no sistema 3DeT Victory.</p>
-                <br />
-                <p style={{color: '#7A7A7A'}}>Você pode baixar o fastplay demo <a href='https://blog.jamboeditora.com.br/wp-content/uploads/2023/08/3DeT-DEMO-Kit-Introdutorio.pdf'>aqui</a>.</p>
+            <div style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <div style={{padding: '8px 0', display: 'flex', flexDirection: 'column', maxWidth: '500px'}}>
+                    <h1 style={{textTransform: 'uppercase'}}>3DeT tacaficha</h1>
+                    <br />
+                    <p style={{color: '#7A7A7A'}}>Este é um site não oficial para auxiliar na criação de personagens no sistema 3DeT Victory.</p>
+                    <br />
+                    <p style={{color: '#7A7A7A'}}>Você pode baixar o fastplay demo <a href='https://blog.jamboeditora.com.br/wp-content/uploads/2023/08/3DeT-DEMO-Kit-Introdutorio.pdf'>aqui</a>.</p>
+                </div>
             </div>
         </>
     )
