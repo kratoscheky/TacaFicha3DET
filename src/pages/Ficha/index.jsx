526a188f5bc31e6b6afd0c09b1db9510ddb24688
Mauricio Soares <kratoscheky@gmail.com>
2023-08-27 22:26:10 -0300

    Correção width do separador dado

--- src/pages/Ficha/index.jsx
+++ src/pages/Ficha/index.jsx
@@ -23,7 +23,7 @@ export const Ficha = () => {
         hiddenFileInput.current.click();
     };
 
-    const SeparadorDadinho = () => <img src={dadinho} alt={"Dados"} />;
+    const SeparadorDadinho = () => <img src={dadinho} style={{width: '100%'}} alt={"Dados"} />;
 
     return (
         <>
