b65c52fa9210da2544853b1f6bd256886edf9310
Lucas Lean <lucaslean@outlook.com.br>
2023-08-29 17:28:49 -0300

    (￣_,￣ ) Adiciona assets holograficos ao projeto

--- src/components/TacaCarta/styles.jsx
+++ src/components/TacaCarta/styles.jsx
@@ -39,8 +39,8 @@ export const Card = styled.div`
 
     &.foil:after {
         opacity: 1;
-        background-image: url("https://assets.codepen.io/13471/sparkles.gif"), 
-            url(https://assets.codepen.io/13471/holo.png), 
+        background-image: url("assets/sparkles.webp"), 
+            url(assets/holo.webp), 
             linear-gradient(125deg, #ff008450 15%, #fca40040 30%, #ffff0030 40%, #00ff8a20 60%, #00cfff40 70%, #cc4cfa50 85%);
         background-position: 50% 50%;
         background-size: cover;
