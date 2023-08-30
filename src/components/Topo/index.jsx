522ca933235c7ac6d3eb0a51e7a082b203528450
Mauricio Soares <kratoscheky@gmail.com>
2023-08-30 00:51:25 -0300

    Novo visual para o topo

--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -1,28 +1,71 @@
 import React from "react";
-import logo from '../../images/logo.png'
+import logo from "../../images/TacaFicha/Logo.svg";
+import bannerPng from "../../images/TacaFicha/Banner.png";
+import tresdetlogo from "../../images/TacaFicha/logo 3DeT.svg";
+import tablet from "../../images/TacaFicha/tablet.png";
 import { DarkModeToggle } from "../DarkModeToggle";
-import { ContainerBarraSuperior } from "./styles";
+import {
+  Button,
+  ContainerBanner,
+  ContainerBarraSuperior,
+  ContainerLogo,
+  ContainerTablet,
+  ContainerTexto,
+  ContainerTextoETablet,
+  Descricao,
+} from "./styles";
+import DownloadIcon from "@mui/icons-material/Download";
 
 export const Topo = () => {
-    return (
-        <>
-            <ContainerBarraSuperior>
-                <img style={{height: '50px', paddingLeft: '8px', paddingTop: '8px'}} src={logo} alt="3DeT TacaFicha"/>
-                <DarkModeToggle />
-            </ContainerBarraSuperior>
-            <div style={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center'
-            }}>
-                <div style={{padding: '8px 8px', display: 'flex', flexDirection: 'column', maxWidth: '500px'}}>
-                    <h1 style={{textTransform: 'uppercase'}}>3DeT tacaficha</h1>
-                    <br />
-                    <p style={{color: 'var(--secondary-text)'}}>Este é um site não oficial para auxiliar na criação de personagens no sistema 3DeT Victory.</p>
-                    <br />
-                    <p style={{color: 'var(--secondary-text)'}}>Você pode baixar o fastplay demo <a href='https://blog.jamboeditora.com.br/wp-content/uploads/2023/08/3DeT-DEMO-Kit-Introdutorio.pdf' style={{color: '#d11ce0'}}>aqui</a>.</p>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <ContainerBarraSuperior>
+        <ContainerLogo>
+          <img style={{ height: "37px" }} src={logo} alt="3DeT TacaFicha" />
+          <div />
+          <img
+            style={{ height: "37px" }}
+            src={tresdetlogo}
+            alt="3DeT TacaFicha"
+          />
+        </ContainerLogo>
+        <DarkModeToggle />
+      </ContainerBarraSuperior>
+      <ContainerBanner
+        style={{
+          backgroundImage: `url(${bannerPng})`,
+        }}
+      >
+        <ContainerTextoETablet>
+          <ContainerTexto>
+            <Descricao>
+              <h1>Olá aventureiro!</h1>
+              <br />
+              <p>
+                Este é um site não oficial para auxiliar na criação de
+                personagens no sistema <b>3DeT Victory</b>.
+              </p>
+            </Descricao>
+
+            <a
+              href="https://blog.jamboeditora.com.br/wp-content/uploads/2023/08/3DeT-DEMO-Kit-Introdutorio.pdf"
+              style={{ textDecoration: "none" }}
+            >
+              <Button>
+                <b>Baixe a versão demo</b>
+                <DownloadIcon
+                  style={{
+                    color: "#FFF",
+                  }}
+                />
+              </Button>
+            </a>
+          </ContainerTexto>
+          <ContainerTablet>
+            {/* <img src={tablet} /> */}
+          </ContainerTablet>
+        </ContainerTextoETablet>
+      </ContainerBanner>
+    </>
+  );
+};
--- src/components/Topo/styles.jsx
+++ src/components/Topo/styles.jsx
@@ -6,8 +6,86 @@ export const ContainerBarraSuperior = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 20px 16px;
 
     button {
         margin-right: 8px;
+    }
+`
+
+export const ContainerLogo = styled.div`
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    div{
+        height: 40px;
+        border: 1px solid #FFF;
+    }
+`
+
+export const ContainerBanner = styled.div`
+    width: 100%;
+    height: 578px;
+    display: flex;
+    justify-content: center;
+    background-size: auto;
+    border-bottom: 10px solid #6F0062;
+`
+
+export const ContainerTextoETablet = styled.div`
+    padding: 8px;
+    display: flex;
+    max-width: 500px;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+`
+
+export const ContainerTablet = styled.div`
+    position: relative;
+    img{
+        position: absolute;
+        position: absolute;
+        right: -110px;
+        top: -289px;
+    }
+`
+
+export const ContainerTexto = styled.div`
+display: flex;
+flex-direction: column;
+gap: 70px;
+    h1{
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        color: #FFF;
+    }
+    p{
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        color: #FFF;
+    }
+`
+
+export const Descricao = styled.div`
+    max-width: 250px;
+`
+
+export const Button = styled.button`
+    display: flex;
+    background: #6F0062;
+    border-radius: 10px;
+    border: none;
+    padding: 12px;
+    align-items: center;
+    b{
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        color: #FFF;
+        text-transform: uppercase;
     }
 `
