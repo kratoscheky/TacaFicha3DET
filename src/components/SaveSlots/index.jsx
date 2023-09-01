a37d0ad8c8a97e632e503746458940d89b0181ba
Mauricio Soares <kratoscheky@gmail.com>
2023-09-01 20:13:10 -0300

    Mensagem de delete nos slots

--- src/components/SaveSlots/index.jsx
+++ src/components/SaveSlots/index.jsx
@@ -1,34 +1,92 @@
 import React from "react";
 import { useSlots } from "../../context/slots.context";
-import { AvisoTexto, Card, ContainerButtons, ContainerCards, ContainerSessao, InnerCard, NomeTopo, StatusContainer } from "./styles";
-import poder from '../../images/poder.svg'
-import habilidade from '../../images/habilidade.svg'
-import resistencia from '../../images/resistencia.svg'
-import IconButton from '@mui/material/IconButton';
-import SimCardDownloadRoundedIcon from '@mui/icons-material/SimCardDownloadRounded';
-import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
+import {
+  AvisoTexto,
+  ButtonsFlex,
+  Card,
+  ConfirmButton,
+  ContainerButtons,
+  ContainerCards,
+  ContainerConteudoModal,
+  ContainerModal,
+  ContainerSessao,
+  DeleteButton,
+  InnerCard,
+  NomeTopo,
+  StatusContainer,
+} from "./styles";
+import poder from "../../images/poder.svg";
+import habilidade from "../../images/habilidade.svg";
+import resistencia from "../../images/resistencia.svg";
+import IconButton from "@mui/material/IconButton";
+import SimCardDownloadRoundedIcon from "@mui/icons-material/SimCardDownloadRounded";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import { Modal } from "@mui/material";
+
+const ModalConfirmarDelete = ({ open, onClose, onDelete }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <ContainerModal onClick={onClose}>
+        <ContainerConteudoModal>
+          <h1>TU VAI DELETA MANO!</h1>
+          <p>
+            Cara, tu tem certeza mesmo que quer deletar teu personagem? Tipo, se
+            vai perder ele sabe! PRA SEMPRE! Você ainda quer deletar ele?
+          </p>
+          <ButtonsFlex>
+            <ConfirmButton onClick={onClose}><p>Eu desisto! Não quero mais</p></ConfirmButton>
+            <DeleteButton onClick={onDelete}><p>Deleta logo!</p></DeleteButton>
+          </ButtonsFlex>
+        </ContainerConteudoModal>
+      </ContainerModal>
+    </Modal>
+  );
+};
 
 export const SaveSlots = () => {
   const { slots, DeleteSlot, LoadSlot } = useSlots();
+  const [open, setOpen] = React.useState(false);
+  const [actualIndex, setActualIndex] = React.useState();
 
-  if(!slots.length){
+  if (!slots.length) {
     return (
       <ContainerSessao>
-        <p style={{color: '#d11ce0'}}>BETA</p>
+        <p style={{ color: "#d11ce0" }}>BETA</p>
         <h1>Personagens Salvos</h1>
         <AvisoTexto>Você ainda não tem nenhum personagem salvo!</AvisoTexto>
       </ContainerSessao>
-    )
+    );
   }
+
+  const handleOpen = (index) => {
+    setActualIndex(index);
+    setOpen(true);
+  };
+
+  const deleteSlot = () => {
+    DeleteSlot(actualIndex);
+    setOpen(false);
+  };
 
   return (
     <ContainerSessao>
-      <p style={{color: '#fff'}}>BETA</p>
+      <p style={{ color: "#fff" }}>BETA</p>
       <h1>Personagens Salvos</h1>
       <br />
       <ContainerCards>
         {slots.map((s, index) => (
-          <Card key={index + s.nome} backgroundImage={s.imageUrl ?? 'https://site.jamboeditora.com.br/wp-content/uploads/2023/07/3DeT-abertura-mobile.png'}>
+          <Card
+            key={index + s.nome}
+            backgroundImage={
+              s.imageUrl ??
+              "https://site.jamboeditora.com.br/wp-content/uploads/2023/07/3DeT-abertura-mobile.png"
+            }
+          >
             <InnerCard>
               <NomeTopo>
                 <p>{s.nome}</p>
@@ -47,12 +105,21 @@ export const SaveSlots = () => {
               </StatusContainer>
             </InnerCard>
             <ContainerButtons>
-              <IconButton onClick={() => DeleteSlot(index)}><DeleteForeverRoundedIcon style={{color: 'Crimson'}} /></IconButton>
-              <IconButton onClick={() => LoadSlot(s)}><SimCardDownloadRoundedIcon style={{color: '#FFF'}} /></IconButton>
+              <IconButton onClick={() => handleOpen(index)}>
+                <DeleteForeverRoundedIcon style={{ color: "Crimson" }} />
+              </IconButton>
+              <IconButton onClick={() => LoadSlot(s)}>
+                <SimCardDownloadRoundedIcon style={{ color: "#FFF" }} />
+              </IconButton>
             </ContainerButtons>
           </Card>
         ))}
       </ContainerCards>
+      <ModalConfirmarDelete
+        open={open}
+        onClose={() => setOpen(false)}
+        onDelete={() => deleteSlot()}
+      />
     </ContainerSessao>
   );
 };
--- src/components/SaveSlots/styles.jsx
+++ src/components/SaveSlots/styles.jsx
@@ -23,12 +23,95 @@ export const ContainerSessao = styled.div`
     }
 `
 
+export const ContainerModal = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 16px;
+`
+
+export const ContainerConteudoModal = styled.div`
+    max-width: 500px;
+    padding: 16px;
+    background: #6F0062;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    h1{
+        color: #FFBF22;
+    }
+    p{
+        font-weight: 500;
+        font-family: Roboto;
+        color: #FFF;
+    }
+`
+
 export const AvisoTexto = styled.p`
     color: #FFF;
     font-style: normal;
     font-weight: 700;
     line-height: normal;
     text-transform: uppercase;
+    width: 100%;
+`
+
+export const DeleteButton = styled.div`
+        font-weight: 600;
+    text-shadow: 0 0 16px rgba(0,0,0,.53);
+    background-color: crimson;
+    box-shadow: 0 0 10px 0 rgba(0,0,0,.5);
+    font-size: 16px;
+    padding: 15px 30px;
+    border-radius: 4px;
+    text-decoration: none;
+    fill: #fff;
+    text-align: center;
+    transition: all .3s;
+    border: none;
+    flex-grow: 1;
+    order: 10;
+    display: inline-block;
+    color: #fff;
+    cursor: pointer;
+    width: 100%;
+    display: flex;
+    gap: 4px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ConfirmButton = styled.div`
+    font-weight: 600;
+    text-shadow: 0 0 16px rgba(0,0,0,.53);
+    background-color: #ffb300;
+    box-shadow: 0 0 10px 0 rgba(0,0,0,.5);
+    font-size: 16px;
+    padding: 15px 30px;
+    border-radius: 4px;
+    text-decoration: none;
+    fill: #fff;
+    text-align: center;
+    transition: all .3s;
+    border: none;
+    flex-grow: 1;
+    order: 10;
+    display: inline-block;
+    color: #fff;
+    cursor: pointer;
+    width: 100%;
+    display: flex;
+    gap: 4px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ButtonsFlex = styled.div`
+    display: flex;
+    gap: 16px;
     width: 100%;
 `
 
