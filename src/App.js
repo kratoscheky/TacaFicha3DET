ba3c2787dcc157d80f62d03ad7244373a87d41ec
Mauricio Soares <kratoscheky@gmail.com>
2023-09-03 17:51:09 -0300

    Atualização de creditos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Topo } from "./components/Topo";
 import { Ficha } from "./pages/Ficha";
 import { ContainerPage } from "./styles";
 import { SaveSlots } from "./components/SaveSlots";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { AvisoTopo } from "./components/AvisoTopo";
 import { ArquetipoPainel } from "./components/ArquetiposPainel";
 
@@ -19,18 +19,54 @@ function App() {
         <Ficha />
       </ContainerPage>
       <div
+        style={{
+          background: "#000",
+          width: "100%",
+          display: "flex",
+          flexDirection: 'column',
+          padding: "16px",
+          justifyContent: "center",
+          alignItems: 'center'
+        }}
+      >
+        <p
           style={{
-            background: "#000",
-            width: "100%",
-            display: 'flex',
-            padding: '16px',
-            justifyContent: 'center'
+            color: "#FFF",
+            textAlign: "center",
           }}
         >
-          <p style={{
-            color: '#FFF',
-            textAlign: 'center'
-          }}>Este site foi criado por <a style={{color: '#D11CE0'}} href='https://twitter.com/Kratoscheky'>@kratoscheky</a> com colaboração de <a style={{color: '#D11CE0'}} href='https://twitter.com/OvelhaDev'>@OvelhaDev</a> e <a style={{color: '#D11CE0'}} href='https://twitter.com/kpoper_cansada'>@kpoper_cansada</a></p>
+          Este site foi criado por{" "}
+          <a
+            style={{ color: "#D11CE0" }}
+            href="https://twitter.com/Kratoscheky"
+          >
+            @kratoscheky
+          </a>{" "}
+          com colaboração de{" "}
+          <a style={{ color: "#D11CE0" }} href="https://twitter.com/OvelhaDev">
+            @OvelhaDev
+          </a>{" "}
+          e{" "}
+          <a
+            style={{ color: "#D11CE0" }}
+            href="https://twitter.com/kpoper_cansada"
+          >
+            @kpoper_cansada
+          </a>
+        </p>
+        <br />
+        <p style={{
+          color: '#FFF'
+        }}>
+          Design feito por
+          <a style={{ color: "#D11CE0", paddingLeft: '8px', paddingRight: '8px' }} href="https://www.facebook.com/groups/161485003862799/user/100086200497493/">
+            Lungas Neto
+          </a>
+          e
+          <a style={{ color: "#D11CE0", paddingLeft: '8px' }} href="https://twitter.com/Kratoscheky">
+            Mauricio Soares
+          </a>
+        </p>
       </div>
     </>
   );
