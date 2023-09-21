8edc1bd9c30e75569d0eacc341ca20261b2ce72a
kratoscheky <kratoscheky@gmail.com>
2023-09-21 19:48:45 -0300

    Ajustes para colinha nos dados

--- src/components/TacaDado/index.jsx
+++ src/components/TacaDado/index.jsx
@@ -1,8 +1,10 @@
 import {CloseI, Container, ContainerButton, Icon} from "./styles";
 import {useState} from "react";
 import IconDice from '../../images/IconDice.png'
+import {useFicha} from "../../context/ficha.context";
 
 export const TacaDado = () => {
+  const {atributos} = useFicha()
   const [exibirDados, setExibirDados] = useState(false);
 
   if(!exibirDados)
@@ -15,7 +17,7 @@ export const TacaDado = () => {
   return (
     <>
       <Container>
-        <iframe src='https://dados.tacaficha.com.br' width='100%' height='100%' />
+        <iframe src={`https://dados.tacaficha.com.br?p=${atributos.poder}&h=${atributos.habilidade}&r=${atributos.resistencia}`} width='100%' height='100%' />
         <CloseI onClick={() => setExibirDados(false)} />
       </Container>
     </>
